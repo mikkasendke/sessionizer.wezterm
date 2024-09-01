f00c055c81b6846708c672940c11d2241bb57dce
mikka <mikka@sendke.eu>
2024-09-01 12:48:08 +0200

    let us see

--- plugin/init.lua
+++ plugin/init.lua
@@ -106,6 +106,7 @@ local function apply_commands(entries)
     for _, dir in pairs(paths) do
         local command = shallow_copy(config.command)
         command[#command + 1] = dir
+        wez.log_info(command)
 
         local success, stdout, _ = wez.run_child_process(command)
         if not success then
