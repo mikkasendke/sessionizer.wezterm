08787bf642171ebb8c468b18a8dadc76a8a9d428
mikka <mikka@sendke.eu>
2024-11-29 23:00:32 +0100

    refactor: rename all_active_sessions options

--- plugin/sessionizer/generators/all_active_sessions.lua
+++ plugin/sessionizer/generators/all_active_sessions.lua
@@ -3,8 +3,8 @@ local generator = {}
 
 -- TODO: Add option to attach (Current) to current workspace
 local default_options = {
-    show_current_workspace = true,
-    show_default_workspace = true,
+    filter_default = true,
+    filter_current = true,
 }
 
 local function get_all_active_workspaces(options)
@@ -19,8 +19,8 @@ local function get_all_active_workspaces(options)
     local current = wez.mux.get_active_workspace()
     local all = wez.mux.get_workspace_names()
     for i, v in ipairs(all) do
-        if not options.show_current_workspace and v == current then goto continue end
-        if not options.show_default_workspace then goto continue end
+        if options.filter_current and v == current then goto continue end
+        if options.filter_default and v == "default" then goto continue end
         table.insert(workspaces, { label = v, id = v })
         ::continue::
     end
