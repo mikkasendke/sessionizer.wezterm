443d2424ac67e48aa373ff2b51a545a2e595b81d
mikka <mikka@sendke.eu>
2025-01-11 21:24:48 +0100

    remove: zoxide is in another plugin soon

--- plugin/sessionizer/generators/index.lua
+++ plugin/sessionizer/generators/index.lua
@@ -4,6 +4,5 @@ builtin.DefaultWorkspace = require "sessionizer.generators.default_workspace".De
 builtin.MostRecentWorkspace = require "sessionizer.generators.most_recent_workspace".MostRecentWorkspace
 builtin.AllActiveWorkspaces = require "sessionizer.generators.all_active_sessions".AllActiveWorkspaces
 builtin.FdSearch = require "sessionizer.generators.fd".FdSearch
-builtin.Zoxide = require "sessionizer.generators.zoxide".Zoxide
 
 return builtin
